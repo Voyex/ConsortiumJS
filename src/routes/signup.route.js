318cc05fe7fa4ea17e7d12e3ee3d1969ee892504
Mitchell Haley <mitchhaley5@gmail.com>
2022-12-16 23:07:09 -0600

    Added signup API

---
+++ src/routes/signup.route.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const conn = require('../configs/connectdb');
+
+router.use('/', async (req, res) => {
+    const email = req.body?.email;
+    const password = req.body?.password;
+
+    if (!(email && password)) {
+        return res.status(400).send({error: "Missing email or password"});
+    }
+
+    const hashedPass = await bcrypt.hash(password, 10);
+
+    const sqlStatment = 'INSERT INTO user (email, password) VALUES (?,?)';
+    conn.execute(sqlStatment, [email, hashedPass]).then((result) => {
+        return res.status(200).send({message: "User created successfully"});
+    }).catch((err) => {
+        return res.status(500).send({error: "Failed to create user"});
+    });
+});
+
+module.exports = router;
