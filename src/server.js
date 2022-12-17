b4f786a8c0f143a299f1de9bf1279be8d5dc5e83
Mitchell Haley <mitchhaley5@gmail.com>
2022-12-16 23:05:49 -0600

    changed location of source file

---
+++ src/server.js
@@ -0,0 +1,32 @@
+// Imports
+const express = require('express');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+const cors = require('cors');
+// Enables use of environment variables
+require('dotenv').config();
+
+// Constants
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+// CORS
+app.use(cors({}));
+// For accepting post data from requests
+app.use(bodyParser.json());
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	}),
+);
+// Register routes
+app.use(routes);
+
+// Start the server
+app.listen(port, () => {
+	console.log(`Listening on port: ${port}`);
+});
+
+module.exports = app;
