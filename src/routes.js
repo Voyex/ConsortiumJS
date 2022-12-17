a115b270245942cb95561324e513f16b407ee388
Mitchell Haley <mitchhaley5@gmail.com>
2022-12-16 23:06:35 -0600

    added routes file

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const loginRoute = require('./routes/login.route');
+const signupRoute = require('./routes/signup.route');
+
+router.use("/login", loginRoute);
+router.use('/signup', signupRoute);
+
+module.exports = router;
