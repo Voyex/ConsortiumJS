ce9d671da6c29e4f5a82993c519f90f6d7a76d35
Mitchell Haley <mitchhaley5@gmail.com>
2022-12-17 18:00:52 -0600

    mostly Finished login API

--- src/routes/login.route.ts
+++ src/routes/login.route.ts
@@ -1,14 +1,18 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+import { conn } from '../configs/connectdb';
+import jwt from 'jsonwebtoken';
+import { UserTokens } from '/user-tokens.type';
 const router = Router();
-import conn from '../configs/connectdb';
 
-router.post('/', async (req, res) => {
+export default router.post('/', async (req: Request, res: Response): Promise<void> => {
   const email = req.body?.email;
   const password = req.body?.password;
 
   // Email and Password are required feilds
   if (!(email && password)) {
-    return res.status(400).send({error: 'Missing email or password'});
+    res.status(400).send({error: 'Missing email or password'});
+    return;
   }
 	
   /* 
@@ -16,19 +20,65 @@ router.post('/', async (req, res) => {
 	compare the passwords
 	respond to the user
 	*/
-  const sqlStatment = 'SELECT password FROM user WHERE password = ?';
+  const sqlStatment = 'SELECT password FROM user WHERE email = ?';
 	
-  let result;
+  let rows: any;
   try {
-    [result] = await conn.query(sqlStatment, [password]);
+    [rows] = await conn.execute(sqlStatment, [email]);
+    // Return an error of the user could not be found
+    if (rows?.length <= 0) {
+      res.status(401).send({error: 'Incorrect email or password'});
+      return;
+    }
   } catch (error) {
-    return res.status(500).send({error: 'Database query failed'});
+    res.status(500).send({error: 'Database query failed'});
+    return;
   }
+  const userData = rows[0] ?? {};
 
-  // Return an error of the user could not be found
-  // if (result?.length <= 0) {
-  //   return res.status(401).send({error: 'Incorrect email or password'});
-  // }
+  try {
+    const isCorrectPass: boolean = await bcrypt.compare(password, userData?.password);
+    if (isCorrectPass) {
+      const tokens = createJWT(userData); 
+      // Set the cookies
+      res.cookie('accessToken', tokens.accessToken, {
+        secure: true,
+        sameSite: 'none',
+        httpOnly: true,
+      });
+      res.cookie('refreshToken', tokens.refreshToken, {
+        secure: true,
+        sameSite: 'none',
+        httpOnly: true,
+      });
+      res.status(200).send({message: 'Logged in successfully'});
+      return;
+    }
+    res.status(401).send({error: 'Incorrect email or password'});
+    return;
+  } catch (error) {
+    res.status(500).send({error: 'Password check failed'});
+    return;
+  }
 });
 
-export default router;
+const createJWT = (payloadData: any): UserTokens => {
+  const accessSecret = process.env?.ACCESS_TOKEN_SECRET ?? '';
+  const refreshSecret = process.env?.REFRESH_TOKEN_SECRET ?? '';
+
+  const userTokens = {} as UserTokens;
+
+  // Generate JWT here
+  userTokens.accessToken = jwt.sign({
+    email: payloadData.email,
+    role: payloadData.role,
+    id: payloadData.id,
+  }, accessSecret, { expiresIn: '10m' });
+  userTokens.refreshToken = jwt.sign({
+    email: payloadData.email,
+    role: payloadData.role,
+    id: payloadData.id,
+  }, refreshSecret, { expiresIn: '1d' });
+
+  return userTokens;
+};
