ae7a35b364752856092ab3a82ea4cc77a3e1201b
Mitchell Haley <haleymj5781@uwec.edu>
2022-12-19 00:18:38 -0600

    adder try catch util to abstract try catches

--- src/utils/tryCatch.ts
+++ src/utils/tryCatch.ts
@@ -1,8 +1,11 @@
 import { Request, Response, NextFunction, Router } from 'express';
-exports.tryCatch = (controller: Router) => async (req: Request, res: Response, next: NextFunction) => {
+
+const tryCatch = (controller: any) => async (req: Request, res: Response, next: NextFunction) => {
   try {
     await controller(req, res, next);
-  } catch (error) {
+  } catch (error: unknown) {
     return next(error);
   }
 };
+
+export default tryCatch;
