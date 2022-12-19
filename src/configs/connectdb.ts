1b527f5af68b8907384947ca44749b60977c4426
Mitchell Haley <haleymj5781@uwec.edu>
2022-12-19 00:15:11 -0600

    added checks for variables and to make sure connection worked

--- src/configs/connectdb.ts
+++ src/configs/connectdb.ts
@@ -3,11 +3,22 @@ import dotenv from 'dotenv';
 // Enable the use of environment variables
 dotenv.config();
 
-export const conn: mysql.Pool = mysql.createPool({
-  host: 'donationmanagement.org',
-  port: 3306,
-  database: 'consortium',
-  user: 'consortium',
-  password: process.env.DB_PASS,
-  connectionLimit: 10,
-});
+if (!process.env.DB_PASS) {
+  throw new Error('Could not Find DB Password');
+}
+
+let conn: mysql.Pool;
+try {
+  conn = mysql.createPool({
+    host: 'donationmanagement.org',
+    port: 3306,
+    database: 'consortium',
+    user: 'consortium',
+    password: process.env.DB_PASS,
+    connectionLimit: 10,
+  });
+} catch (error) {
+  throw new Error('Could not Connect to Database');
+}
+
+export default conn;
