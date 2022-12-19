4020e7aa93e5da93f0cd1cc2a1e154f3b2396534
Mitchell Haley <haleymj5781@uwec.edu>
2022-12-19 17:54:53 -0600

    moved database info to .env file

--- src/configs/connectdb.ts
+++ src/configs/connectdb.ts
@@ -3,19 +3,28 @@ import dotenv from 'dotenv';
 // Enable the use of environment variables
 dotenv.config();
 
-if (!process.env.DB_PASS) {
+if (
+  !(
+    process.env.DB_HOST &&
+    process.env.DB_PORT &&
+    process.env.DB_NAME &&
+    process.env.DB_USER &&
+    process.env.DB_PASS &&
+    process.env.DB_CONN_LIMIT
+  )
+) {
   throw new Error('Could not Find DB Password');
 }
 
 let conn: mysql.Pool;
 try {
   conn = mysql.createPool({
-    host: 'donationmanagement.org',
-    port: 3306,
-    database: 'consortium',
-    user: 'consortium',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    connectionLimit: 10,
+    connectionLimit: parseInt(process.env.DB_CONN_LIMIT),
   });
 } catch (error) {
   throw new Error('Could not Connect to Database');
