72cfa1c060393c638f1677a85739db3664c14676
Mitchell Haley <haleymj5781@uwec.edu>
2022-12-19 00:17:47 -0600

    added user data type

---
+++ src/models/user/user-data.model.ts
@@ -0,0 +1,15 @@
+export class UserData {
+  public id: number | null;
+  public name: string | null;
+  public email: string | null;
+  public password: string | null;
+  public bio: string | null;
+
+  constructor() {
+    this.id = null;
+    this.name = null;
+    this.email = null;
+    this.password = null;
+    this.bio = null;
+  }
+}
