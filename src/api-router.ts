452bf1129b1026c8b111a3d3630b5bf9d27c5a64
Mitchell Haley <haleymj5781@uwec.edu>
2022-12-19 00:13:27 -0600

    updated routes

--- src/api-router.ts
+++ src/api-router.ts
@@ -2,8 +2,8 @@ import { Router } from 'express';
 const router: Router = Router();
 
 // Import routes
-import loginRoute from './routes/login.route';
-import signupRoute from './routes/signup.route';
+import loginRoute from './routes/login.route.js';
+import signupRoute from './routes/signup.route.js';
 
 router.use('/login', loginRoute);
 router.use('/signup', signupRoute);
